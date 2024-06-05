21bcaeb18f1b9eb86f71731eeaab4f2de7e1030c
thomassmith0526 <thomassmith0526@gmail.com>
2024-06-05 12:08:23 -0500

    data

--- Texas Weather/JS/texas.js
+++ Texas Weather/JS/texas.js
@@ -1,16 +1,82 @@
-const submit = document.getElementById("submit");
-const tdyWea = document.getElementById("todayweather");
-const weaDiv = document.getElementById("main-weather");
+const submit = document.querySelector("#submit");
+const tdyWea = document.querySelector(".tdyDiv");
+const weekDiv = document.querySelector("#main-weather");
 const apiKey = "3d8d7e3772c59d486c6023acf03a4346";
-// let cityStorage = JSON.parse(localStorage.getItem("cities"));
+const userCity = document.getElementById("city");
+const tdyDate = dayjs().format('MM DD, YYYY')
+console.log(tdyDate)
+let cityStorage = JSON.parse(localStorage.getItem("cities"));
 
-// if (cityStorage == null) {
- let cityStorage = [];
-// }
+if (cityStorage == null) {
+}
+
+function todayweather(data) {
+  const tdyMainDiv = document.createElement("div");
+  tdyMainDiv.setAttribute("class", "col-12 pt-5 ps-4 bg-light");
+  tdyWea.append(tdyMainDiv);
+  console.log('todayweather', data)
+
+  const cityName = document.createElement("h2");
+  console.log('hellow')
+  cityName.textContent = `City: ${data[0].name}` ;
+  tdyMainDiv.append(cityName);
+  
+  const tdyTemp = document.createElement('h2');
+  console.log(data)
+  tdyTemp.textContent =  `Temp: ${geodata.list[0].main.temp} `;
+  tdyMainDiv.append(tdyTemp);
+
+  const tdyWind = document.createElement('h3');
+  tdyWind.textContent =  `Wind: mph`;
+  tdyMainDiv.append(tdyWind)
+
+  const tdyHum = document.createElement('h3');
+  tdyHum.textContent = `Humidity:`;
+  tdyMainDiv.append(tdyHum)
+}
+
+
+
+
+
+
+
+
+
+function initial() {
+  weaDiv.textContent = ""
+  tdyWea.textContent = ""
+}
+
+function weather(data) {
+  initial()
+  weekWeather(data)
+  todayweather(data)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 submit.addEventListener("click", function () {
   console.log("submit");
-  const city = document.getElementById("city").value;
+  const city = userCity.value;
+  let cityStorage =[]
+  //   let lat = 29.4252
+  // let lon = -98.4946
+  // let city = 'San Antonio'
   fetch(
     ` https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
   )
@@ -18,30 +84,27 @@ submit.addEventListener("click", function () {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-
-      const mainDiv = document.createElement("div");
-      mainDiv.setAttribute("class", "col-2 pt-5 ps-4 ");
-      tdyWea.append(mainDiv);
-
-      const cityName = document.createElement("h3");
-      // cityName.textContent = `${}`
+      // console.log(data);
+      getWeather(data);
+      todayweather(data);
     });
+  function getWeather(geodata) {
+    console.log('geodata');
+    let lat = geodata[0].lat;
+    let lon = geodata[0].lon;
+    // const sampleGeodata = [{lat: 40.7128, lon: -74.0060}]
+    // console.log(sampleGeodata)
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (geodata) {
+        console.log(geodata);
+        // tdyWea(data);
+        // fivedayforcast(data);
+      });
+  }
 });
-
-// function getWeather(geodata) {
-//   let lat = geodata[0].lat;
-//   let lon = geodata[0].lon;
-
-//   fetch(
-//     `https://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&appid=${apiKey}`
-//   )
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       userCity(data);
-//       fivedayforcast(data);
-//     });
-// }
