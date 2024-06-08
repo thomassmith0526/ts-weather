407dce10dc00206f40dede067740e79882264ca9
thomassmith0526 <thomassmith0526@gmail.com>
2024-06-07 21:33:28 -0500

    finsh 5day

--- Texas Weather/JS/texas.js
+++ Texas Weather/JS/texas.js
@@ -3,16 +3,13 @@ const tdyWea = document.querySelector(".tdyDiv");
 const weekDiv = document.querySelector("#main-weather");
 const apiKey = "3d8d7e3772c59d486c6023acf03a4346";
 const userCity = document.getElementById("city");
-const tdyDate = dayjs().format("MM DD, YYYY");
+const tdyDate = dayjs().format("MM,DD,YYYY");
 console.log(tdyDate);
-  let cityStorage = JSON.parse(localStorage.getItem("cities"));
 
-  if (cityStorage == null) {
-  }
 submit.addEventListener("click", function () {
   console.log("submit");
-  const city = userCity.value;
-  let cityStorage = [];
+  // const city = userCity.value;
+  // let cityStorage = [];
   //   let lat = 29.4252
   // let lon = -98.4946
   // let city = 'San Antonio'
@@ -43,32 +40,48 @@ submit.addEventListener("click", function () {
       .then(function (geodata) {
         console.log(geodata);
         todayweather(geodata);
-        // fivedayforcast(geodata);
+        fivedayforcast(geodata);
       });
   }
 
-
   function tdycity(data) {
-    
     const tdyMainDiv = document.createElement("div");
-    tdyMainDiv.setAttribute("class", "col-12 pt-5 ps-4 bg-light");
+    tdyMainDiv.setAttribute("class", "col-12 pt-5 ps-4");
     tdyWea.append(tdyMainDiv);
-    console.log("city", data); 
+    console.log("city", data);
 
-     const cityName = document.createElement("h2");
+    const cityName = document.createElement("h2");
     console.log("hello");
-    cityName.textContent = `City: ${data[0].name}`;
+    cityName.textContent = `City: ${data[0].name} ${tdyDate}`;
     tdyMainDiv.append(cityName);
   }
-  
-  function todayweather(geodata){ 
-     let realTemp = ((Number(geodata.list[0].main.temp)- 273.15)* 9/5 +32).toFixed(0)
-     const front = 'https://openweathermap.org/img/wn/'
-     const back = '@2x.png'
-     console.log(geodata)
-    const mainDiv = document.createElement("div")
-    mainDiv.setAttribute("class", "col-12 pt-5 ps-4 bg-light")
-    tdyWea.append(mainDiv)
+
+  function todayweather(geodata) {
+    let realTemp = (
+      ((Number(geodata.list[0].main.temp) - 273.15) * 9) / 5 +
+      32
+    ).toFixed(0);
+    let wEmoji = "☀️";
+    switch (geodata.list[0].weather[0].main) {
+      case "Rain":
+        wEmoji = "🌧️";
+        break;
+      case "Clouds":
+        wEmoji = "☁️";
+        break;
+      default:
+        wEmoji = "☀️";
+    }
+
+    console.log(geodata);
+    const mainDiv = document.createElement("div");
+    mainDiv.setAttribute("class", "col-12 pt-5 ps-4 ");
+    tdyWea.append(mainDiv);
+
+    const icon = document.createElement("p");
+    icon.textContent = `${wEmoji}`;
+    mainDiv.append(icon);
+
     const tdyTemp = document.createElement("h2");
     console.log(geodata);
     tdyTemp.textContent = `Temp: ${realTemp}F `;
@@ -81,49 +94,96 @@ submit.addEventListener("click", function () {
     const tdyHum = document.createElement("h3");
     tdyHum.textContent = `Humidity: ${geodata.list[0].main.humidity}%`;
     mainDiv.append(tdyHum);
-
-    const icon = document.createElement("img");
-    icon.setAttribute("src ${geodata.list[0].weather[0].icon}")
-    icon.textContent = `${geodata.list[0].weather[0].icon}`;
-    mainDiv.append(icon)
   }
 
-  // function fivedayforcast(geodata){
-  //   console.log(geodata)
-  //   for (let i=4; i<40; i+8){
-  //     let realTemp = ((Number(geodata.list[0].main.temp)- 273.15)* 9/5 +32).toFixed(0)
-  //     let wEmoji = '☀️'
-  //     switch(geodata.list[i].weather[0].main) {
-  //       case 'Rain':
-  //         wEmoji = '🌧️'
-  //         break;
-  //       case 'Clouds':
-  //         wEmoji = '☁️'
-  //         break;
-  //     default:
-  //         wEmoji = '☀️' 
-  //     }
-  //     const tdyCard = document.createElement("div")
-  //     tdyCard.setAttribute("class", "col-2 bg-info")
-  //     weekDiv.append(tdyCard)
-     
+  function fivedayforcast(geodata) {
+    console.log(geodata);
+    for (let i = 4; i < 40; i = i + 8) {
+      // console.log(i);
+      let realTemp = (
+        ((Number(geodata.list[i].main.temp_max) - 273.15) * 9) / 5 +
+        32
+      ).toFixed(0);
+      let wEmoji = "☀️";
+      switch (geodata.list[i].weather[0].main) {
+        case "Rain":
+          wEmoji = "🌧️";
+          break;
+        case "Clouds":
+          wEmoji = "☁️";
+          break;
+        default:
+          wEmoji = "☀️";
+      }
+      const tdyCard = document.createElement("div");
+      tdyCard.setAttribute("class", "col-2 p-3 m-2 ");
+      weekDiv.append(tdyCard);
 
-  //     const dayDate = document.createElement('h3')
-  //     dayDate.textContent =` ${geodata.list[i].main.dx_txt}`
+      const dayDate = document.createElement("h3");
+      // console.log("hello");
+      dayDate.textContent = ` ${dayjs(geodata.list[i].dt_txt).format(
+        "MM/DD/YYYY"
+      )}`;
+      tdyCard.append(dayDate);
 
+      const dayEmo = document.createElement("p");
+      dayEmo.textContent = `${wEmoji}`;
+      tdyCard.append(dayEmo);
 
-  //   }
+      const dayTemp = document.createElement("h3");
+      dayTemp.textContent = `${realTemp}F`;
+      tdyCard.append(dayTemp);
 
-  // }
-  
+      const dayWind = document.createElement("h3");
+      dayWind.textContent = `${geodata.list[i].wind.speed}mph`;
+      tdyCard.append(dayWind);
+
+      const dayHum = document.createElement("h3");
+      // console.log('god lord')
+      dayHum.textContent = `${geodata.list[i].main.humidity}%`;
+      tdyCard.append(dayHum);
+    }
+  }
 
   // function initial() {
-  //   weaDiv.textContent = "";
-  //   tdyWea.textContent = "";
+  //   tdyCard.textContent = "";
+  //   mainDiv.textContent = "";
   // }
 
-  // function weather(data) {
+  // function weather(geodata) {
   //   initial();
-  //   weekWeather(data);
-  //   todayweather(data);
+  //   fivedayforcast(geodata);
+  //   todayweather(geodata);
+  // }
+  // weather();
+  
+  
+  
+  
+  
 });
+
+// const city = document.getElementById(submit)
+function cityhistory() {
+  // console.log(submit.value)
+  // if (submit.value == null){
+  //   console.log(true)
+  //   return
+  // } else {
+  //   console.log(false)
+  // }
+
+}
+cityhistory();
+// let cityStorage = JSON.parse(localStorage.getItem("cities"));
+
+// if (cityStorage == null) {
+// }
+  // $(document).ready(function(){
+  //   $("#city").submit(function(event){
+  //     event.preventDefault();
+  //     let inputValues = $("#city").val()
+  //     console.log("Captured input value:" + inputValues);
+  //     $("#city").val('')
+  //   })
+  // })
